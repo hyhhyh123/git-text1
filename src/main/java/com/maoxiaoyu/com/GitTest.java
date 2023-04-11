50949bab371f97758cf499da4d4a9659bd605e2a
hyh <2714690577@qq.com>
2023-04-11 23:58:38 +0800

    hot-fix commit1

--- src/main/java/com/maoxiaoyu/com/GitTest.java
+++ src/main/java/com/maoxiaoyu/com/GitTest.java
@@ -7,5 +7,6 @@ package com.maoxiaoyu.com;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git22222222");
     }
 }
