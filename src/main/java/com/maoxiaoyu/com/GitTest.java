83d7603a8f961b60e975d1cfeba6c3acf43a677e
hyh <2714690577@qq.com>
2023-04-11 23:44:28 +0800

    first commit

---
+++ src/main/java/com/maoxiaoyu/com/GitTest.java
@@ -0,0 +1,11 @@
+package com.maoxiaoyu.com;
+
+/**
+ * @version 17.0
+ * @auther 猫小鱼
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
